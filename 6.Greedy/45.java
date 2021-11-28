391c052a214219c43d5283bf18b0dc115ac04891
PeterWright <peterwrighten@gmail.com>
2021-11-28 21:21:36 +0900

    update: 45.java

---
+++ 6.Greedy/45.java
@@ -0,0 +1,32 @@
+import java.lang.Math;
+
+
+public class canJump  
+{
+    public int Jump(int[] nums)
+    {
+        int size = nums.length;
+        int mstpos = 0;
+        int end = 0;
+        int step = 0;
+        for(int i = 0; i < size; i++)
+        {
+            if(mstpos >= i)
+            {
+                mstpos = Math.max(mstpos, i + nums[i]);
+                if( i == end )
+                {
+                    end = mstpos;
+                    step++;
+                }
+            }
+        }
+        return step;
+    }
+    public static void main(String[] args)
+    {
+        canJump c = new canJump();
+        int[] nums = {1, 5, 6, 8, 4, 6, 3, 7};
+        System.out.println(c.Jump(nums));
+    }
+}
