933197af8cb596449bc11457d2dfecd5b3b8590f
PeterWright <peterwrighten@gmail.com>
2022-01-13 14:27:26 +0900

    update::21mergeTwoLists

---
+++ 1. Recursion/21mergeTwoLists.cc
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    class ListNode {
+    public:
+        int val;
+        ListNode* next;
+    };
+    ListNode* l1, l2;
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr) return l2;
+        else if(l2 == nullptr) return l1;
+        else if(l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
