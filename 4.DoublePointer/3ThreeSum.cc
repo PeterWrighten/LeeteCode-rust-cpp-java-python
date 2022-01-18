efaec9d966235108c935466f2c8b591f660955cf
PeterWright <peterwrighten@gmail.com>
2022-01-19 00:24:51 +0900

    update::ThreeSum

---
+++ 4.DoublePointer/3ThreeSum.cc
@@ -0,0 +1,39 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int first = 0; first < n; ++first) {
+            if(first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            int third = n - 1;
+            int target = -nums[first];
+            for(int second = first+1; second < n; ++second) {
+                if(second > first+1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                while(second < third && nums[second] + nums[third] > target) {
+                    third--;
+                }
+
+                if(second == third) break;
+
+                if(nums[second] + nums[third] == target) {
+                    vector<int> list;
+                    list.push_back(nums[first]);
+                    list.push_back(nums[second]);
+                    list.push_back(list[third]);
+                    ans.push_back(list);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
