ce4df004cba2c9df9e4006427aaea0e7934b077b
PeterWright <peterwrighten@gmail.com>
2022-01-20 01:11:06 +0900

    update::219::cpp

---
+++ 3.DSA/219containsDup.cc
@@ -0,0 +1,18 @@
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        int n = nums.size();
+        for(int i = 0; i < n; ++i){
+            if(map.count(nums[i]) && i - map[nums[i]] <= k) {
+                return true;
+            }
+            map[nums[i]] = i;
+        }
+        return false;
+    }
+};
