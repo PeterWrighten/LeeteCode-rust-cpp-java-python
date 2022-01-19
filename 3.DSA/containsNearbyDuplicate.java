b2f1096922af5ff18f60c2d8ce39e8cfde01edbd
PeterWright <peterwrighten@gmail.com>
2022-01-20 01:04:29 +0900

    update::containsNearbySup

---
+++ 3.DSA/containsNearbyDuplicate.java
@@ -0,0 +1,40 @@
+// LeeteCode 219
+
+
+import java.util.HashMap;
+import java.util.Map;
+// import java.lang.Math;
+
+// Brute Force:
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         int n = nums.length;
+//         for(int i = 0; i < n; i++) {
+//             int j = i+1;
+//             while(Math.abs(i - j) <= k && j < n) {
+//                 if(nums[i] == nums[j]) {
+//                     return true;
+//                 }
+//                 j++;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// HashMap:
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < n; i++) {
+            if(map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
