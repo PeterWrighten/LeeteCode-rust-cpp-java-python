70a84faae9540551d112d7e5932d7d47088e74af
PeterWright <peterwrighten@gmail.com>
2022-01-25 23:20:06 +0900

    update::1688

---
+++ 7.Math/1688numOfMatches.rs
@@ -0,0 +1,18 @@
+struct solution;
+
+impl solution {
+    pub num_of_matches(n: i32) -> i32 {
+        let mut num = n;
+        let mut ans = 0;
+        while num != 1 {
+            if num % 2 == 0 {
+                ans += num / 2;
+                num = num / 2;
+            } else {
+                ans += (num - 1) / 2;
+                num = (num - 1) / 2 + 1;
+            }
+        }
+        ans
+    }
+}
