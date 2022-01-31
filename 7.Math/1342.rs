5681b372a1f818138718ca60d64fabda1d70382e
PeterWright <peterwrighten@gmail.com>
2022-02-01 01:39:43 +0900

    update::math::1342

---
+++ 7.Math/1342.rs
@@ -0,0 +1,16 @@
+struct solution;
+impl solution {
+    pub fn number_of_steps(mut num: i32) -> i32 {
+        let mut count = 0;
+        loop {
+            if num == 0 { break; }
+            count += 1;
+            if num % 2 == 0 {
+                num = num / 2;
+            } else {
+                num -= 1;
+            }
+        }
+        count
+    }
+}
