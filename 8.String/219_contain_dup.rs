fe3f046f7dfeec03a54f612747e0c88091c1675b
PeterWright <peterwrighten@gmail.com>
2022-02-12 02:16:15 +0900

    update::contain_dup

---
+++ 8.String/219_contain_dup.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+struct solution {};
+
+impl solution {
+    fn contain_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut map = HashMap::new();
+        for (idx, &num) in nums.iter().enumerate() {
+            match map.get(&num) {
+                Some(v) => {
+                    if idx - v <= k {
+                        return true;
+                    }
+                    map.insert(num, idx);
+                }
+
+                None => {
+                    map.insert(num, idx);
+                }
+            }
+        }
+        false
+        
+    }
+}
