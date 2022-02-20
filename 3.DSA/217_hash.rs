8ea9c038a815cafa32f238a63cd461794ce31025
PeterWrighten <peterwrighten@gmail.com>
2022-02-21 01:00:27 +0900

    update::DSA::Hash

---
+++ 3.DSA/217_hash.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+struct Solution {}
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut hash: HashMap<i32, i32> = HashMap::new();
+        for &val in nums.iter() {
+            if let Some(val) = hash.get(&val) {
+                return true;
+            }
+            hash.insert(val, val);
+        }
+        false
+    }
+}
