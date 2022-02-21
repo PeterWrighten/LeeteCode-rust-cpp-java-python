832318f4dd4a303343dc5f40afd6ab71bdca0307
PeterWrighten <peterwrighten@gmail.com>
2022-02-22 07:53:36 +0900

    update::test_module

--- rust_leetecode/src/brute_force/s1984_mindiff.rs
+++ rust_leetecode/src/brute_force/s1984_mindiff.rs
@@ -1,7 +1,7 @@
 pub struct Solution{}
 
 impl Solution {
-    fn min_diff(nums: Vec<i32>, k: i32) -> i32 {
+    pub fn min_diff(nums: Vec<i32>, k: i32) -> i32 {
         let mut nums = nums;
         nums.sort();
         let mut ans = i32::MAX;
@@ -10,4 +10,14 @@ impl Solution {
         }
         ans
     }
+ }
+
+ #[cfg(test)]
+ mod tests {
+     use super::*;
+     #[test]
+     fn test_s1984() {
+         assert_eq!(0, Solution::min_diff(vec![90], 1));
+         assert_eq!(2, Solution::min_diff(vec![9, 4, 1, 7], 2));
+     }
  }
--- rust_leetecode/src/string/s0219_contain_dup.rs
+++ rust_leetecode/src/string/s0219_contain_dup.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-struct Solution {}
+pub struct Solution {}
 
 impl Solution {
     pub fn contain_duplicate(nums: Vec<i32>, k: i32) -> bool {
@@ -8,7 +8,7 @@ impl Solution {
         for (idx, &num) in nums.iter().enumerate() {
             match map.get(&num) {
                 Some(v) => {
-                    if idx - v <= k {
+                    if idx - v <= k as usize {
                         return true;
                     }
                     map.insert(num, idx);
@@ -21,5 +21,16 @@ impl Solution {
         }
         false
         
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    #[test]
+    fn test_s0219() {
+        assert_eq!(true, Solution::contain_duplicate(vec![1, 2, 3, 1], 3));
+        assert_eq!(true, Solution::contain_duplicate(vec![1, 0, 1, 1], 1));
+        assert_eq!(false, Solution::contain_duplicate(vec![1, 2, 3, 1, 2, 3], 2));
     }
 }
