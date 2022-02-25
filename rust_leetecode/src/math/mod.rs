b0b924fb9f1646214c66ebfdcc6ed3b5267ecbd6
PeterWrighten <peterwrighten@gmail.com>
2022-02-26 00:38:53 +0900

    update::math::matrix_shape

--- rust_leetecode/src/math/mod.rs
+++ rust_leetecode/src/math/mod.rs
@@ -2,3 +2,4 @@ mod s0540_single_nonDup;
 mod s1342_num_of_step;
 mod s1688_num_of_matches;
 mod s1984_mindiff;
+mod s0566_matrix_reshape;
---
+++ rust_leetecode/src/math/s0566_matrix_reshape.rs
@@ -0,0 +1,20 @@
+pub struct Solution {}
+ impl Solution {
+     pub fn matrix_reshape(mat: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
+         if mat.len() * mat[0].len() == (r * c) as usize {
+             mat.concat().chunks(c as usize).map(|v| v.to_vec()).collect()
+         } else {
+             mat
+         }
+     }
+ }
+
+ #[cfg(test)]
+ mod tests {
+     use super::*;
+     #[test]
+     fn test_s0566() {
+        assert_eq!(vec![vec![1, 2, 3, 4]], Solution::matrix_reshape(vec![vec![1, 2], vec![3, 4]], 1, 4));
+        assert_eq!(vec![vec![1, 2], vec![3, 4]], Solution::matrix_reshape(vec![vec![1, 2], vec![3, 4]], 2, 2));
+     }
+ }
