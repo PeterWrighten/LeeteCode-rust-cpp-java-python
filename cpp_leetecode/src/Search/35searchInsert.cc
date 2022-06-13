6cdbe929a3224232af56037f33e0bae9a5ae34c1
PeterWrighten <peterwrighten@gmail.com>
2022-06-14 03:21:41 +0900

    Update: cpp_leetecode::35

---
+++ cpp_leetecode/src/Search/35searchInsert.cc
@@ -0,0 +1,21 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int mid, ans;
+        while(lo <= hi) {
+            mid = lo + (hi - lo) >> 1;
+            if(target >= nums[mid]) {
+                ans = mid + 1;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
